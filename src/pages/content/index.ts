576eda0afb06e2d0d4d4bf1d5997cf2bedcb1548
rlagudals95 <ahovmf@naver.com>
2023-08-23 22:45:29 +0900

    feat: inject css

--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,17 +1,17 @@
 import { CustomElement } from "./elements/customElement";
 import { appendToBody } from "./utils/dom";
 
-const popup = new CustomElement({ tagName: "div" });
+// const popup = new CustomElement({ tagName: "div" });
 
-const siteDetectContent = new CustomElement({
-  tagName: "div",
-  styles: { position: "fixed", right: "30px", top: "30px" },
-});
-siteDetectContent.setText("정신 차려 이친구야");
+// const siteDetectContent = new CustomElement({
+//   tagName: "div",
+//   styles: { position: "fixed", right: "30px", top: "30px" },
+// });
+// siteDetectContent.setText("정신 차려 이친구야");
 
-popup.addChild(siteDetectContent.getElement());
+// popup.addChild(siteDetectContent.getElement());
 
-appendToBody(popup.getElement());
+// appendToBody(popup.getElement());
 
 // personal
 
@@ -24,6 +24,7 @@ const personalSchedulePopup = new CustomElement({
     padding: "1.5rem 1rem",
     width: "fit-content",
   },
+  classes: ["personal-popup"],
 });
 
 const schedules = ["connact 개발", "위탁/구매대행 공부", "머신러닝 공부"];
